64d9934934d4c3b168d10cbe1c9b0a791c50c42f
André Marques <andre.m.d.abreu@gmail.com>
2024-02-01 11:34:34 -0300

    Primeiras alterações

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Eita doidera</h1>
+      <Menu />
+      <main>
+        <h2>uma hora sai</h2>
+        <p>calmai</p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Opa, bão?</h1>
+      <Menu />
+      <main>
+        <h2>blablabla</h2>
+        <p>teste</p>
+      </main>
+    </>
+  );
+}
