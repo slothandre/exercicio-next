65efbe8e6628540811f8ddf4a7629eb9061f8e75
André Marques <andre.m.d.abreu@gmail.com>
2024-02-04 22:50:05 -0300

    Create localizacoes.jsx

---
+++ src/pages/localizacoes.jsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import styled from "styled-components";
+import serverApi from "./api/server";
+
+export async function getStaticProps() {
+  try {
+    const resposta = await fetch(`${serverApi}/location`);
+    const dados = await resposta.json();
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+    return {
+      props: { dados },
+    };
+  } catch (error) {
+    console.error("Ops, deu ruim: " + error.message);
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Localizacoes({ dados }) {
+  if (!dados || !dados.results) {
+    return <p>Carregando...</p>;
+  }
+
+  const { results } = dados;
+
+  if (!results || !results.lenght === 0) {
+    return <p>Localizações não encontradas</p>;
+  }
+  return (
+    <>
+      <Head>
+        <title>Rico e Morto - Localizacoes</title>
+      </Head>
+      <h2>Localizações</h2>
+      <StyledLocalizacoes></StyledLocalizacoes>
+    </>
+  );
+}
+const StyledLocalizacoes = styled.section``;
